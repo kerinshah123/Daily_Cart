e298e353102e1536f45e7c66ad03ab97046a2f9c
Arshdeep kaur <karshdeep52@gmail.com>
2020-08-08 11:43:21 -0700

    about us and start activity added

---
+++ app/src/main/java/com/example/dailycart/StartActivity.java
@@ -0,0 +1,53 @@
+
+package com.example.dailycart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+    Button login , signin ;
+    TextView skip ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        login =findViewById(R.id. login );
+        signin =findViewById(R.id. signup );
+        skip =findViewById(R.id. skip );
+
+        login .setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),MainActivity. class );
+                i.putExtra( "name" , login .getText().toString());
+                startActivity(i);
+            }
+        });
+        signin .setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),MainActivity. class );
+                i.putExtra( "name" , signin .getText().toString());
+                startActivity(i);
+            }
+        });
+        skip .setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(),homeActivity. class );
+                startActivity(intent);
+            }
+        });
+    }
+
+}
