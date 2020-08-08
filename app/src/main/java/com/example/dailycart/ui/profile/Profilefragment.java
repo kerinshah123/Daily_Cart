f4868ead11b63e77d2bee0eec3da16ff6a7b5897
keyurmistry <keyurmistry96@gmail.com>
2020-08-08 14:12:31 -0400

    user profile

--- app/src/main/java/com/example/dailycart/ui/profile/Profilefragment.java
+++ app/src/main/java/com/example/dailycart/ui/profile/Profilefragment.java
@@ -23,11 +23,11 @@ public class Profilefragment extends Fragment {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+
         profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
             }
         });
         return root;
