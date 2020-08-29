15759483848225fa271db487012bed10daf67907
Arshdeep kaur <karshdeep52@gmail.com>
2020-08-28 22:13:26 -0700

    Payment page

---
+++ app/src/main/java/com/example/dailycart/Payment.java
@@ -0,0 +1,13 @@
+package com.example.dailycart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Payment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
